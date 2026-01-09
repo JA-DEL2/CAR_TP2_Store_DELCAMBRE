86c17a70ef0c778e83dcf19554d8801a1a358963
Delcambre Jean-Alexis <jean-alexis.delcambre.etu@univ-lille.fr>
2026-01-09 10:42:38 +0100

    Init project

---
+++ src/test/java/car/tp1/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package car.tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
